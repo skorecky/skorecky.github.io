f1c85b3db80e1c803df98587de9ab4aa78387512
Stephen Korecky <skorecky@gmail.com>
2012-10-19 23:11:09 -0500

    adding mobile support

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,11 +2,15 @@
   "use strict";
   
   window.onload = function(){
+    
+    window.scrollTo(0, 1);
+    
     var emailButton = document.querySelector("a[href='#email']");
     emailButton.onclick = function(event){
       event.preventDefault();
       window.location = "mailto:"+emailButton.dataset.user+"@gmail.com";
     }
+    
   };
   
 })();
